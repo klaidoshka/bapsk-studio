8c24ccb922b801b58b61687b26b1180cb2025190
klaidas <klaidukast17@gmail.com>
2025-03-16 17:32:40 +0200

    feat/vat-return-submission: Return correct messages on VAT return error; Display messages component background within one card

--- Backend/Accounting-Services/Service/VatReturnService.cs
+++ Backend/Accounting-Services/Service/VatReturnService.cs
@@ -70,7 +70,7 @@ public class VatReturnService : IVatReturnService
         {
             throw new ValidationException(
                 clientResponse.Errors
-                    .Select(e => e.Details)
+                    .Select(e => e.Description)
                     .ToList()
             );
         }
@@ -93,7 +93,7 @@ public class VatReturnService : IVatReturnService
         {
             throw new ValidationException(
                 clientResponse.Errors
-                    .Select(e => e.Details)
+                    .Select(e => e.Description)
                     .ToList()
             );
         }
