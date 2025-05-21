038a2880cffdf585c37434e5145287c0b57d971d
klaidas <klaidukast17@gmail.com>
2025-05-21 12:40:40 +0300

    feat: Use .env variable for support email

--- Web/src/application/environments/app-environment.ts
+++ Web/src/application/environments/app-environment.ts
@@ -1,4 +1,5 @@
 export default interface AppEnvironment {
   ASPNET__SERVER__API: string;
+  SUPPORT__EMAIL: string;
   WEB__SERVER__API: string;
 }
--- Web/src/application/environments/environment.ts
+++ Web/src/application/environments/environment.ts
@@ -2,5 +2,6 @@ import AppEnvironment from "./app-environment";
 
 export const environment: AppEnvironment = {
   ASPNET__SERVER__API: '',
+  SUPPORT__EMAIL: '',
   WEB__SERVER__API: ''
 };
