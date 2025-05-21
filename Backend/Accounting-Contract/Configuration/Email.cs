1718398c07072839ec2c45858251172119f549b4
klaidas <klaidukast17@gmail.com>
2025-05-21 15:43:14 +0300

    feat: Add ResendAPI for Email sending via HTTPS

--- Backend/Accounting-Contract/Configuration/Email.cs
+++ Backend/Accounting-Contract/Configuration/Email.cs
@@ -3,23 +3,30 @@ namespace Accounting.Contract.Configuration;
 public class Email
 {
     public bool EnableSsl { get; set; }
-    public string FromEmail { get; set; }
-    public string FromName { get; set; }
-    public string Host { get; set; }
-    public string Password { get; set; }
-    public int Port { get; set; }
-    public EmailResetPassword ResetPassword { get; set; }
-    public string Username { get; set; }
-    public VatReturnStatusChange VatReturnStatusChange { get; set; }
+    public string FromEmail { get; set; } = "";
+    public string FromName { get; set; } = "";
+    public string Host { get; set; } = "";
+    public string Password { get; set; } = "";
+    public int Port { get; set; } 
+    public Resend Resend { get; set; } = new();
+    public EmailResetPassword ResetPassword { get; set; } = new();
+    public string Username { get; set; } = "";
+    public VatReturnStatusChange VatReturnStatusChange { get; set; } = new();
 }
 
 public class EmailResetPassword
 {
-    public string Endpoint { get; set; }
-    public string Secret { get; set; }
+    public string Endpoint { get; set; } = "";
+    public string Secret { get; set; } = "";
+}
+
+public class Resend
+{
+    public string ApiKey { get; set; } = "";
+    public bool Enabled { get; set; }
 }
 
 public class VatReturnStatusChange
 {
-    public string Endpoint { get; set; }
+    public string Endpoint { get; set; } = "";
 }
